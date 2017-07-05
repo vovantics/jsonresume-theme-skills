9cd891fbd573d0e6b6b381404ba396944628d4e3
stevo <stephenvovan@gmail.com>
2017-07-05 12:44:10 -0400

    First commit.

---
+++ index.js
@@ -0,0 +1,32 @@
+var fs = require("fs");
+var path = require('path');
+var Handlebars = require("handlebars");
+var HandlebarsIntl = require("handlebars-intl");
+HandlebarsIntl.registerWith(Handlebars);
+
+function render(resume) {
+  var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+  var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+  var partialsDir = path.join(__dirname, 'partials');
+  var filenames = fs.readdirSync(partialsDir);
+
+  filenames.forEach(function (filename) {
+    var matches = /^([^.]+).hbs$/.exec(filename);
+    if (!matches) {
+      return;
+    }
+    var name = matches[1];
+    var filepath = path.join(partialsDir, filename)
+    var template = fs.readFileSync(filepath, 'utf8');
+
+    Handlebars.registerPartial(name, template);
+  });
+  return Handlebars.compile(tpl)({
+    css: css,
+    resume: resume
+  });
+}
+
+module.exports = {
+  render: render
+};
